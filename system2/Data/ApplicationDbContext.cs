11432ce6cbef06348736117845dd4feb62a1fc3a
Daniel Eriksson <41974921+jumperstumper@users.noreply.github.com>
2022-02-01 11:09:08 +0100

    Update ApplicationDbContext.cs

--- system2/Data/ApplicationDbContext.cs
+++ system2/Data/ApplicationDbContext.cs
@@ -22,25 +22,7 @@ namespace system2.Data
 
 
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
 
-        //    //Many to Many Project and Users
-        //    base.OnModelCreating(modelBuilder);
-        //    modelBuilder.Entity<ProjectUsers>().HasKey(ba => new { ba.Project_Id, ba.UserId });
-
-        //    modelBuilder.Entity<ProjectUsers>().HasOne(b => b.Project).WithMany(b => b.ProjectUsers).HasForeignKey(b => b.Project_Id);
-        //    modelBuilder.Entity<ProjectUsers>().HasOne(b => b.ApplicationUser).WithMany(b => b.ProjectUsers).HasForeignKey(b => b.UserId);
-
-        //    //Many to Many Users and Ticket
-        //    base.OnModelCreating(modelBuilder);
-        //    modelBuilder.Entity<UserTicket>().HasKey(ba => new { ba.UserId, ba.Ticket_Id });
-
-        //    modelBuilder.Entity<UserTicket>().HasOne(b => b.ApplicationUser).WithMany(b => b.UserTicket).HasForeignKey(b => b.UserId);
-        //    modelBuilder.Entity<UserTicket>().HasOne(b => b.Ticket).WithMany(b => b.UserTicket).HasForeignKey(b => b.Ticket_Id);
-
-
-        //}
 
     }
 }
